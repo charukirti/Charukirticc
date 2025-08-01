9c00769c63bd05f9bbdf1394a324940a435aa489
charukirti <charukirticc112002@gmail.com>
2025-08-01 12:36:31 +0530

    feat(SEO): Added meta tags

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,36 @@ import Skills from "./components/Skills";
 export default function App() {
   return (
     <div className="mx-auto min-h-screen  py-12 md:py-15 lg:py-0 flex flex-col items-center relative">
+      <title>Charukirti Chougule- Frontend Developer</title>
+
+      <meta
+        name="description"
+        content="Frontend Developer skilled in React, TypeScript, and modern web development. Browse my projects and get in touch for collaboration."
+      />
+      <meta
+        name="keywords"
+        content="Charukirti, Frontend Developer, React Developer, React JS, TypeScript, JavaScript, Web Developer, Portfolio"
+      />
+      <meta name="author" content="Charukirti" />
+      <meta name="robots" content="index, follow" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Charukirti - Frontend Developer" />
+      <meta property="og:url" content="https://charukirti.vercel.app" />
+      <meta property="og:site_name" content="Charukirti's Portfolio" />
+      <meta
+        name="og:description"
+        content="Frontend Developer skilled in React, TypeScript, and modern web development. Browse my projects and get in touch for collaboration."
+      />
+
+      <link rel="canonical" href="https://charukirti.vercel.app" />
+
       <Header />
       <AboutMe />
       <Navigation />
       <Skills />
       <Projects />
-      <ContactMe/>
+      <ContactMe />
     </div>
   );
 }
